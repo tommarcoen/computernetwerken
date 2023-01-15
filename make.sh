0293c62187ed1fe4ff37d19b79a3fe034c8c2757
Tom Marcoen <tom@marcoen.net>
2023-01-15 19:10:40 +0100

    Kill all output from the make.sh script

--- make.sh
+++ make.sh
@@ -3,20 +3,20 @@
 compile_book()
 {
    echo "Building the book..."
-   pdflatex main-book > /dev/null
-   makeindex main-book > /dev/null
-   makeglossaries main-book > /dev/null
-   biber main-book > /dev/null
-   pdflatex main-book > /dev/null
-   pdflatex main-book > /dev/null
-   pdflatex main-book > /dev/null
+   pdflatex main-book &> /dev/null
+   makeindex main-book &>/dev/null
+   makeglossaries main-book &> /dev/null
+   biber main-book &> /dev/null
+   pdflatex main-book &> /dev/null
+   pdflatex main-book &> /dev/null
+   pdflatex main-book &> /dev/null
 }
 
 compile_presentation()
 {
    echo "Building the presentation..."
-   pdflatex main-presentation > /dev/null
-   pdflatex main-presentation > /dev/null
+   pdflatex main-presentation &> /dev/null
+   pdflatex main-presentation &> /dev/null
 }
 
 if [ "$1" == "clean" ]; then
