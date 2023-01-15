c7aed43095631aba82f5ba8ce7d24ec94489e5ca
Tom Marcoen <tom@marcoen.net>
2023-01-15 18:59:42 +0100

    Improve the make.sh script

--- make.sh
+++ make.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
-pdflatex main-book
-makeindex main-book
-makeglossaries main-book
-biber main-book
-pdflatex main-book
-pdflatex main-book
-pdflatex main-book
 
-pdflatex main-presentation
-pdflatex main-presentation
+compile_book()
+{
+   echo "Building the book..."
+   pdflatex main-book > /dev/null
+   makeindex main-book > /dev/null
+   makeglossaries main-book > /dev/null
+   biber main-book > /dev/null
+   pdflatex main-book > /dev/null
+   pdflatex main-book > /dev/null
+   pdflatex main-book > /dev/null
+}
 
-grep -i warning *.log
+compile_presentation()
+{
+   echo "Building the presentation..."
+   pdflatex main-presentation > /dev/null
+   pdflatex main-presentation > /dev/null
+}
+
+if [ "$1" == "clean" ]; then
+   git clean -Xf
+elif [ "$1" == "book" ]; then
+   compile_book
+elif [ "$1" == "presentation" ]; then
+   compile_presentation
+else
+   compile_book
+   compile_presentation
+fi
