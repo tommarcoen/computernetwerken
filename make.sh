700df178af0bde409a4251fc76daaf6d55245024
Tom Marcoen <tom.marcoen@gmail.com>
2023-02-08 02:45:44 +0100

    Finish indexing for intro, chap 1 and 2

--- make.sh
+++ make.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
 
+make_clean()
+{
+   echo "Removing untracked files from the working tree..."
+   git clean -Xf
+}
 compile_book()
 {
    echo "Building the book..."
-   ./make.sh clean
-   pdflatex main-book &> /dev/null
-   #makeindex main-book &>/dev/null
-   makeglossaries main-book &> /dev/null
-   biber main-book &> /dev/null
-   pdflatex main-book &> /dev/null
-   pdflatex main-book &> /dev/null
-   pdflatex main-book &> /dev/null
+   pdflatex main-book
+   pdflatex main-book
+   makeindex main-book
+   makeglossaries main-book
+   biber main-book
+   pdflatex main-book
+   pdflatex main-book
+   pdflatex main-book
 }
 
 compile_presentation()
 {
    echo "Building the presentation..."
-   pdflatex main-presentation &> /dev/null
-   pdflatex main-presentation &> /dev/null
+   pdflatex main-presentation
+   pdflatex main-presentation
 }
 
 if [ "$1" = "clean" ]; then
-   echo "Removing untracked files from the working tree..."
-   git clean -Xf &> /dev/null
+   make_clean
 elif [ "$1" = "book" ]; then
+   make_clean
    compile_book
 elif [ "$1" = "presentation" ]; then
+   make_clean
    compile_presentation
 else
+   make_clean
    compile_book
    compile_presentation
 fi
